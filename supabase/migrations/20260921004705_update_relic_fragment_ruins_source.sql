a273a58d6d6eebe8157d8c66b7ca7d65f8b51408
fushidarana888 <chektilo@mail.ru>
2026-09-21 03:47:08 +0300

    Указать руины источником осколка реликвии

---
+++ supabase/migrations/20260921004705_update_relic_fragment_ruins_source.sql
@@ -0,0 +1,3 @@
+update public.item_definitions
+set description='Фрагмент неизвестного древнего изделия, найденный среди древних руин или в глубинах подземелий.'
+where slug='ancient_relic_fragment_beta';
